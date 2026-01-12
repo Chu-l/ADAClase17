2576de2ae3e62e1fed8224801143d9e0fd00ad88
Lilen <lilen.chu@hotmail.com>
2026-01-11 21:21:35 -0300

    Ejercicio 3 terminado

--- actividades.js
+++ actividades.js
@@ -53,7 +53,7 @@ Pide al usuario que ingrese una oración. Luego, escribe la función “procesar
 3. Reemplace todas las vocales 'a' por '@'. Tener en cuenta utilizar “/a/g” que indica que se debe buscar todas las apariciones del carácter 'a' en la cadena (la bandera g es para global, lo que significa que reemplazará todas las apariciones, no solo la primera).
 4. Encuentre la posición de la primera aparición de la palabra "javascript". De no aparecer retornar -1
 5. Convierta la oración a una cadena de palabras separadas por guiones.*/
-
+/*
 // Pedir la oración al usuario
 let oracion = prompt("Ingrese una oración: ");
 // Función
@@ -75,8 +75,43 @@ function procesarOracion(texto) {
 };
 // Invocación
 procesarOracion(oracion);
+*/ 
 
-/**/
+/*Ejercicio 3: Gestión Compleja de Arrays
+A partir del siguiente array de productos, escriba la función ‘gestionarProductos’ que realice las siguientes tareas:
+1. Añada un nuevo producto al array.
+2. Elimine el último producto del array.
+3. Encuentre el índice de un producto específico. En este punto pueden usar forEach o investigar el método “findIndex()”
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/findIndex
+4. Verifique si existe un producto con precio mayor a 50. Para esto investigar el método “.some()” https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/some
+5. Devuelva una cadena de nombres de productos separados por comas.*/
+
+// Array de productos inicial
+let arrayProductos = [
+    { nombre: "Nutella", precio: 9500 },
+    { nombre: "Provoleta", precio: 6000 },
+    { nombre: "Café", precio: 17000 },
+    { nombre: "Pan Lactal", precio: 7500 }
+];
+// Función
+function gestionarProductos(productos) {
+    // 1. Añadir un nuevo producto
+    productos.push({ nombre: "Huevos", precio: 3090 });
+    // 2. Eliminar el último producto
+    productos.pop();
+     // 3. Encontrar el índice de un producto específico
+    let indiceNutella = productos.findIndex(producto => producto.nombre === "Nutella");
+     // 4. Verificar si existe un producto con precio mayor a 50
+    let hayProductoCaro = productos.some(producto => producto.precio > 50);
+    // 5. Devolver una cadena con los nombres de productos separados por comas
+    let nombresProductos = productos.map(producto => producto.nombre).join(", ");
+    //Resultados
+    console.log("El índice de la nutella es ", indiceNutella);
+    console.log("Precio mayor a 50 ", hayProductoCaro);
+    console.log("Lista de productos: ", nombresProductos);
+};
+// Invocación
+gestionarProductos(arrayProductos);
 
 /**/
 
